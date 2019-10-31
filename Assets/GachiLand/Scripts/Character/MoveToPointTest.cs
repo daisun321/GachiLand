e99c96d2806e9d4bd68f0366fc4ff55f3045c7b2
Physical.Linkage <Physical.Linkage@icloud.com>
2019-10-31 20:48:35 +0900

    add move to point

---
+++ Assets/GachiLand/Scripts/Character/MoveToPointTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MoveToPoint))]
+public class MoveToPointTest : MonoBehaviour
+{
+    private MoveToPoint moveToPoint;
+
+    private void Awake()
+    {
+        moveToPoint = GetComponent<MoveToPoint>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var hits = Physics.RaycastAll(ray);
+            foreach (var hit in hits)
+            {
+                if (hit.collider.CompareTag("Ground"))
+                {
+                    moveToPoint.SetTarget(hit.point);
+                    break;
+                }
+            }
+        }
+    }
+}
